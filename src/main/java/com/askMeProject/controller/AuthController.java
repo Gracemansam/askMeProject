83cf6a3de9fb43dde8d209b8a37c8e8d3a1c9eaa
Adetoye <samuel.adetoye@thepalladiumgroup.com>
2024-03-20 05:52:18 -0700

    controller-fix

--- src/main/java/com/askMeProject/controller/AuthController.java
+++ src/main/java/com/askMeProject/controller/AuthController.java
@@ -1,15 +1,12 @@
 package com.askMeProject.controller;
 
+
 import com.askMeProject.common_constant.CommonConstant;
 import com.askMeProject.dto.LoginRequest;
-import com.askMeProject.dto.PasswordRequestUtil;
 import com.askMeProject.dto.UserDTO;
-import com.askMeProject.dto.response.LoginResponse;
-import com.askMeProject.model.User;
+import com.askMeProject.dto.response.AuthResponse;
 import com.askMeProject.model.VerificationToken;
-import com.askMeProject.service.impl.AuthServiceImplementation;
-import io.swagger.annotations.ApiOperation;
-import jakarta.mail.MessagingException;
+import com.askMeProject.service.implementation.UserServiceImplementation;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,92 +14,61 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.UnsupportedEncodingException;
-import java.util.Optional;
-import java.util.UUID;
-
 @RestController
 @RequestMapping("/auth")
+
 public class AuthController {
 
+    private final UserServiceImplementation userServiceImplementation;
+
     @Autowired
-    private AuthServiceImplementation authServiceImplementation;
-
-
-
-    @ApiOperation(value = "register", notes = "This method is used for user registration")
+    public AuthController(UserServiceImplementation userServiceImplementation) {
+        this.userServiceImplementation = userServiceImplementation;
+    }
 
     @PostMapping("/register-patient")
-    public ResponseEntity<UserDTO> registerPatient(@Valid @RequestBody UserDTO userDTO, HttpServletRequest request){
-        userDTO = authServiceImplementation.registerPatient(userDTO, request);
-        return new ResponseEntity<>(userDTO, HttpStatus.CREATED);
+    public ResponseEntity<AuthResponse> registerPatient(@Valid @RequestBody UserDTO userDto, HttpServletRequest request){
+
+            // Call the createUserHandler method from the service layer
+            ResponseEntity<AuthResponse> responseEntity = userServiceImplementation.registerPatient(userDto,request);
+            return new ResponseEntity<>(responseEntity.getBody(), HttpStatus.OK);
+
     }
 
     @PostMapping("/register-doctor")
-    public ResponseEntity<UserDTO> registerDoctor(@Valid @RequestBody UserDTO userDTO, HttpServletRequest request){
-        userDTO = authServiceImplementation.registerDoctor(userDTO, request);
-        return new ResponseEntity<>(userDTO, HttpStatus.CREATED);
+    public ResponseEntity<AuthResponse> registerDoctor(@Valid @RequestBody UserDTO userDto, HttpServletRequest request){
+
+        // Call the createUserHandler method from the service layer
+        ResponseEntity<AuthResponse> responseEntity = userServiceImplementation.registerDoctor(userDto,request);
+        return new ResponseEntity<>(responseEntity.getBody(), HttpStatus.OK);
+
     }
 
 
-    @PostMapping(path = "/login", consumes = {"application/json"}, produces = {"application/json"})
-    public ResponseEntity<LoginResponse> login(@Valid @RequestBody LoginRequest loginRequest){
-        LoginResponse login  = authServiceImplementation.login(loginRequest.getEmail(), loginRequest.getPassword());
-        return new ResponseEntity<>(login, HttpStatus.OK);
+    @PostMapping("/signin")
+    public ResponseEntity<AuthResponse> signIn(@Valid @RequestBody LoginRequest loginRequest) {
+        try {
+            // Call the signIn method from the service layer
+            ResponseEntity<AuthResponse> responseEntity = userServiceImplementation.signIn(loginRequest);
+            return new ResponseEntity<>(responseEntity.getBody(), HttpStatus.OK);
+        } catch (Exception e) {
+            // Handle exceptions and return an error response
+            return new ResponseEntity<>(new AuthResponse(e.getMessage() , false), HttpStatus.UNAUTHORIZED);
+        }
     }
 
     @GetMapping("/verifyEmail")
     public String verifyEmail(@RequestParam("token") String token){
-        VerificationToken theToken = authServiceImplementation.findByToken(token);
+        VerificationToken theToken = userServiceImplementation.findByToken(token);
         if (theToken.getUser().isEnabled()){
             return CommonConstant.EXISTING_USER_VERIFICATION_MESSAGE;
         }
-        String verificationResult = authServiceImplementation.validateToken(token);
+        String verificationResult = userServiceImplementation.validateToken(token);
         if (verificationResult.equalsIgnoreCase("valid")){
             return CommonConstant.VERIFICATION_MESSAGE_C;
         }
         return CommonConstant.TOKEN_NOT_VALID;
     }
 
-    @PostMapping("/password-reset-request")
-    public String resetPasswordRequest(@RequestBody PasswordRequestUtil passwordRequestUtil,
-                                       final HttpServletRequest servletRequest)
-            throws MessagingException, UnsupportedEncodingException {
-
-        Optional<User> user = authServiceImplementation.findByEmail(passwordRequestUtil.getEmail());
-        String passwordResetUrl = "";
-        if (user.isPresent()) {
-            String passwordResetToken = UUID.randomUUID().toString();
-            authServiceImplementation.createPasswordResetTokenForUser(user.get(), passwordResetToken);
-            passwordResetUrl = passwordResetEmailLink(user.get(), applicationUrl(servletRequest), passwordResetToken);
-        }
-        return passwordResetUrl;
-    }
-
-    private String passwordResetEmailLink(User user, String applicationUrl,
-                                          String passwordToken) throws MessagingException, UnsupportedEncodingException {
-        String url = applicationUrl+"/register/reset-password?token="+passwordToken;
-      //  eventListener.sendPasswordResetVerificationEmail(url);
-       // log.info("Click the link to reset your password :  {}", url);
-        return url;
-    }
-    @PostMapping("/reset-password")
-    public String resetPassword(@RequestBody PasswordRequestUtil passwordRequestUtil,
-                                @RequestParam("token") String token){
-        String tokenVerificationResult = authServiceImplementation.validatePasswordResetToken(token);
-        if (!tokenVerificationResult.equalsIgnoreCase("valid")) {
-            return "Invalid token password reset token";
-        }
-        Optional<User> theUser = Optional.ofNullable(authServiceImplementation.findUserByPasswordToken(token));
-        if (theUser.isPresent()) {
-            authServiceImplementation.changePassword(theUser.get(), passwordRequestUtil.getNewPassword());
-            return "Password has been reset successfully";
-        }
-        return "Invalid password reset token";
-    }
-    public String applicationUrl(HttpServletRequest request) {
-        return "http://"+request.getServerName()+":"
-                +request.getServerPort()+request.getContextPath();
-    }
 
 }
---
+++ src/main/java/com/askMeProject/controller/ForgetPasswordController.java
@@ -0,0 +1,53 @@
+package com.askMeProject.controller;
+
+
+import com.askMeProject.service.implementation.ForgetPasswordService;
+import com.askMeProject.service.implementation.UserServiceImplementation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/forgetPassword")
+@RequiredArgsConstructor
+
+public class ForgetPasswordController {
+    private  final UserServiceImplementation userServiceImplementation;
+
+    private final ForgetPasswordService forgetPasswordService;
+
+    @PostMapping("/send-otp")
+    public String sendOtp(@RequestParam String email) {
+        try {
+            forgetPasswordService.initiateForgetPassword(email);
+            return "OTP sent successfully";
+        } catch (Exception e) {
+            return "Error sending OTP: " + e.getMessage();
+        }
+    }
+
+    @PostMapping("/verify-otp")
+    public String verifyOtp(@RequestParam String email, @RequestParam String enteredOtp) {
+        try {
+            if (forgetPasswordService.verifyOtp(email, enteredOtp)) {
+                forgetPasswordService.passwordResetLink(email, enteredOtp);
+                return "OTP verified successfully, and password reset link sent to email";
+            } else {
+                return "Invalid OTP";
+            }
+        } catch (Exception e) {
+            return "Error verifying OTP: " + e.getMessage();
+        }
+    }
+
+    @PostMapping("/reset-password/{email}/{otp}")
+    public String resetPassword(@PathVariable String email, @PathVariable String otp, @RequestParam String newPassword) {
+        try {
+            forgetPasswordService.resetPassword(email, otp,newPassword);
+            return "Password reset successfully";
+        } catch (Exception e) {
+            return "Error resetting password: " + e.getMessage();
+        }
+    }
+
+
+}
